80ed7bc1fe89bec90395520153a7c51bb5cb7011
Luis Peña <iskejaf@outlook.com>
2020-04-11 18:37:23 -0400

    CRUD Ready

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,30 +1,38 @@
 const notesCtrl = {};
+const note = require('../models/note');
 
 // All Notes
-notesCtrl.renderNotes = (req, res) => {
-    res.send('All Notes');
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await note.find().lean();
+    res.render('notes/all-notes', { notes });
 };
 
 // New Note
 notesCtrl.renderNewNote = (req, res) => {
     res.render('notes/new-note');
 };
-notesCtrl.newNote = (req, res) => {
-    console.log(req.body);
-    res.send('Create new');
+notesCtrl.newNote = async (req, res) => {
+    const {title, description} = req.body;
+    const newNote = new note({title, description});
+    await newNote.save();
+    res.redirect('/notes');
 };
 
 // Edit Note
-notesCtrl.renderEditNote = (req, res) => {
-    res.send('Edit Note');
+notesCtrl.renderEditNote = async (req, res) => {
+    const editNote = await note.findById(req.params.id).lean();
+    res.render('notes/edit-note', { editNote });
 };
-notesCtrl.editNote = (req, res) => {
-    res.send('edit');
+notesCtrl.editNote = async (req, res) => {
+    const { title, description } = req.body;
+    await note.findByIdAndUpdate(req.params.id, {title, description}).lean();
+    res.redirect('/notes');
 };
 
 // Delete Note
-notesCtrl.deleteNote = (req, res) => {
-    res.send('edit');
+notesCtrl.deleteNote = async (req, res) => {
+    await note.findByIdAndDelete(req.params.id).lean();
+    res.redirect('/notes');
 };
 
 module.exports = notesCtrl;
